be6d15f43c28b3fe2c6b4e317b414cd9190f3ff6
Konstantin Dolgov <konstantin.dolgov@crm-integpro.ru>
2024-04-23 09:33:04 +0300

    Init project files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <p className="title">Kastel Mihalson portfolio</p>
+      <p className="description">Coming soon</p>
+    </div>
+  );
+}
+
+export default App;
